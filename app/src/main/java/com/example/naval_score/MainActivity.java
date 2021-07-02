a852565ed3e264190f225cdbe08e7fbe5edaefa0
NavalSharma7 <A00241484@myCambrian.ca>
2021-07-02 22:22:15 +0530

    functionality working
    added toggle button for team selection, full functionality working with toggle button selectors.
    
    score less than 0 handled.

--- app/src/main/java/com/example/naval_score/MainActivity.java
+++ app/src/main/java/com/example/naval_score/MainActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView teamAScore;
     private TextView teamBScore;
+    private ToggleButton teamSwitch;
     private RadioButton onePoint;
     private RadioButton twoPoint;
     private RadioButton threePoint;
@@ -31,12 +34,18 @@ public class MainActivity extends AppCompatActivity {
         onePoint = findViewById(R.id.rb_one_point);
         twoPoint = findViewById(R.id.rb_two_point);
         threePoint = findViewById(R.id.rb_three_points);
+        teamSwitch = findViewById(R.id.tb_team);
+
     }
 
     public void onClickPlus(View view) {
-        String scoreA = teamAScore.getText().toString();
-        if (!TextUtils.isEmpty(scoreA)) {
-            int score = Integer.parseInt(scoreA);
+
+        String scoreStr = teamAScore.getText().toString();
+        if(teamSwitch.isChecked()){
+             scoreStr = teamBScore.getText().toString();
+        }
+        if (!TextUtils.isEmpty(scoreStr)) {
+            int score = Integer.parseInt(scoreStr);
             if (onePoint.isChecked()) {
                 score = score + 1;
             } else if (twoPoint.isChecked()) {
@@ -44,26 +53,42 @@ public class MainActivity extends AppCompatActivity {
             } else if (threePoint.isChecked()) {
                 score = score + 3;
             }
-            teamAScore.setText(String.format("%s", score));
-        } else teamAScore.setText("1");
+            if(teamSwitch.isChecked()){
+                teamBScore.setText(String.format("%s", score));
+            }else{
+            teamAScore.setText(String.format("%s", score));}
+        } else {
+            if(teamSwitch.isChecked()){
+                teamBScore.setText("1");
+            }else{
+            teamAScore.setText("1");
+    }}
     }
 
     public void onClickMinus(View view) {
-        String scoreA = teamAScore.getText().toString();
-        if (!TextUtils.isEmpty(scoreA)) {
-            int score = Integer.parseInt(scoreA);
+        String scoreStr = teamAScore.getText().toString();
+        if(teamSwitch.isChecked()){
+            scoreStr = teamBScore.getText().toString();
+        }
+        if (!TextUtils.isEmpty(scoreStr)) {
+            int score = Integer.parseInt(scoreStr);
             if (onePoint.isChecked()) {
                 score = score - 1;
-
             } else if (twoPoint.isChecked()) {
                 score = score - 2;
             } else if (threePoint.isChecked()) {
                 score = score - 3;
             }
-            if(score < 0){
-                score = 0;
-            }
+            if(teamSwitch.isChecked()){
+                teamBScore.setText(String.format("%s", score));
+            }else{
             teamAScore.setText(String.format("%s", score));
-        } else teamAScore.setText("1");
+        }} else {
+            if(teamSwitch.isChecked()){
+                teamBScore.setText("1");
+            }else{
+            teamAScore.setText("1");
+        }
+        }
     }
 }
